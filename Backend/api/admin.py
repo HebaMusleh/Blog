a6ed125e5190c86eba5f4b230fd27eec2e0557be
hebamesluh1 <hebamesluh@gmail.com>
2025-02-27 13:30:24 +0200

    fix : fixed images issue

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import User,Profile,Category,Post
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['title']}
 
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
